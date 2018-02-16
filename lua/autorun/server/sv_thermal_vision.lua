f2ae5128252b307766c879fc819c294dfede041c
Kuma96 <36542670+Kuma96@users.noreply.github.com>
2018-02-16 17:04:10 +0100

    Create sv_thermal_vision.lua

---
+++ lua/autorun/server/sv_thermal_vision.lua
@@ -0,0 +1,16 @@
+util.AddNetworkString("thermal_vision_darkrp_jobs")
+local thermal_vision_job_list = {}
+
+--ADD, REMOVE OR CHANGE JOBS HERE
+--You have to find yourself the index number of the job you want to add here
+--This example shows the index number of some jobs ON THE VANILLA GAMEMODE only
+thermal_vision_job_list[0] = 2 --TEAM_POLICE
+thermal_vision_job_list[1] = 6 --TEAM_MEDIC
+thermal_vision_job_list[2] = 8 --TEAM_MAYOR
+
+local function thermal_vision_send_jobs(ply)
+	net.Start("thermal_vision_darkrp_jobs")
+	net.WriteTable(thermal_vision_job_list)
+	net.Send(ply)
+end
+hook.Add("PlayerInitialSpawn", "thermal_vision_send_jobs", thermal_vision_send_jobs)
